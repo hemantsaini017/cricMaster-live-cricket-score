aec70c8843c648922eb367542869293070d42adb
hemantsaini017 <itshemantsaini2004@gmail.com>
2025-06-27 13:53:04 +0530

    Delete cricMaster live scrore tracker directory

--- cricMaster live scrore tracker/script.js
+++
@@ -1,7 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offscreenmenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offscreenmenu.classList.toggle('active');
-});
